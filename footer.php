957a649101e2a2b69e7cd5e2b174c5ee37e7d736
Jefffvvv <105692008+Jefffvvv@users.noreply.github.com>
2023-05-24 12:58:30 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,3 @@
 <div class="fixed-bottom">
-        <footer class="p-3 bg-dark text-light d-flex justify-content-center"><p>Copyright &copy; <b><a href="https://www.instagram.com/_reyhannzz/">@_reyhannzz</a></b> All right reserved.</p></footer>
+        <footer class="p-3 bg-dark text-light d-flex justify-content-center"><p>UKL Lancar : Liburan Tenang. Coded by Khanza XI TKJ 1 18</p></footer>
     </div>
