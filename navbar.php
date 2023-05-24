1b6ecd6ad65a643c06ca7a8ab70b7fdd87e2ae11
Jefffvvv <105692008+Jefffvvv@users.noreply.github.com>
2023-05-24 13:02:53 +0700

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -17,7 +17,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
             </a>
-            <a class="nav-link" href="siswa.php">Student data
+            <a class="nav-link" href="siswa.php">data buku
             </a>
             </ul>
             <form action="" method="post">
