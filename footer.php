3c92cdf3ca7b5e567cb2be00ca5fa78d151c9d09
Jefffvvv <105692008+Jefffvvv@users.noreply.github.com>
2023-05-24 23:04:22 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,3 @@
 <div class="fixed-bottom">
-        <footer class="p-3 bg-dark text-light d-flex justify-content-center"><p>UKL Lancar : Liburan Tenang. Coded by Khanza XI TKJ 1 18</p></footer>
+        <footer class="p-3 bg-dark text-light d-flex justify-content-center"><p>UKL Lancar : Liburan Tenang. This Website Was Made by Khanza XI TKJ 1 18</p></footer>
     </div>
